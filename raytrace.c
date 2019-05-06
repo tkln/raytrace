65d2b01d74a9dbc3084f7afca28d51b2c7d49848
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-05-06 21:50:02 +0300

    Render multiple balls

--- raytrace.c
+++ raytrace.c
@@ -1,3 +1,4 @@
+#include <float.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
@@ -5,6 +6,29 @@
 #include "fb.h"
 #include "ray.h"
 #include "vecmat/vec3f.h"
+
+#define ARRAY_LEN(a) (sizeof(a) / sizeof((a)[0]))
+
+struct sphere {
+    struct vec3f orig;
+    float r;
+} spheres[] = {
+    {
+        .orig = { -0.5f, 0.0f, -0.5f },
+        .r = 0.5f,
+    }, {
+        .orig = { 0.0f, 0.0f, -1.0f },
+        .r = 0.5f,
+    }, {
+        .orig = { 0.5f, 0.0f, -1.5f },
+        .r = 0.5f,
+    },
+};
+
+struct hit {
+    struct vec3f p;
+    struct vec3f n;
+};
 
 float hit_sphere(struct vec3f center, float r, struct ray ray)
 {
@@ -18,22 +42,46 @@ float hit_sphere(struct vec3f center, float r, struct ray ray)
     return (-b - sqrtf(discriminant)) / (2.0f * a);
 }
 
+static bool test_hit(struct ray ray, struct hit *hit)
+{
+    size_t min_i;
+    float min_t = FLT_MAX;
+    size_t i;
+    float t;
+
+    for (i = 0; i < ARRAY_LEN(spheres); ++i) {
+        t = hit_sphere(spheres[i].orig, spheres[i].r, ray);
+        if (t < 0.0f)
+            continue;
+        if (t < min_t) {
+            min_t = t;
+            min_i = i;
+        }
+    }
+
+    if (min_t != FLT_MAX) {
+        hit->p = ray_point_along(ray, min_t);
+        hit->n = vec3f_normalized(vec3f_sub(hit->p, spheres[min_i].orig));
+        return true;
+    }
+    return false;
+}
+
 static struct color get_ray_color(struct ray ray)
 {
     struct vec3f unit_dir = vec3f_normalized(ray.dir);
-    struct vec3f sphere_orig = { 0, 0, -1 };
     struct vec3f c;
-    struct vec3f n;
+    struct hit hit;
+    bool is_hit;
     float t;
 
-    t = hit_sphere(sphere_orig, 0.5, ray);
-    if (t > 0.0f) {
-        n = vec3f_normalized(vec3f_sub(ray_point_along(ray, t),
-                                       sphere_orig));
-        c = vec3f_muls(vec3f_adds(n, 1.0f), 0.5f);
+    is_hit = test_hit(ray, &hit);
+    if (is_hit) {
+        c = vec3f_muls(vec3f_adds(hit.n, 1.0f), 0.5f);
         return (struct color) { c.x, c.y, c.z };
     }
 
+    /* Background color */
     t = 0.5f * (unit_dir.y + 1.0f);
     c = vec3f_add(vec3f_init(1.0f - t, 1.0f - t, 1.0f - t),
                   vec3f_muls(vec3f_init(0.5f, 0.7f, 1.0f), t));
