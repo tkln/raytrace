a697dc1da64fa7cf9340fd7f768c179b1fd8a239
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-05-06 18:30:25 +0300

    Calculate and visualize normals

--- raytrace.c
+++ raytrace.c
@@ -21,12 +21,18 @@ float hit_sphere(struct vec3f center, float r, struct ray ray)
 static struct color get_ray_color(struct ray ray)
 {
     struct vec3f unit_dir = vec3f_normalized(ray.dir);
-    float t;
+    struct vec3f sphere_orig = { 0, 0, -1 };
     struct vec3f c;
+    struct vec3f n;
+    float t;
 
-    t = hit_sphere(vec3f_init(0, 0, -1), 0.5, ray);
-    if (t > 0.0f)
-        return (struct color) { 0.2f, 0.3f, 0.5f };
+    t = hit_sphere(sphere_orig, 0.5, ray);
+    if (t > 0.0f) {
+        n = vec3f_normalized(vec3f_sub(ray_point_along(ray, t),
+                                       sphere_orig));
+        c = vec3f_muls(vec3f_adds(n, 1.0f), 0.5f);
+        return (struct color) { c.x, c.y, c.z };
+    }
 
     t = 0.5f * (unit_dir.y + 1.0f);
     c = vec3f_add(vec3f_init(1.0f - t, 1.0f - t, 1.0f - t),
