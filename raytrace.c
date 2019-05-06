e76847a68c27b308224c97b0c61a64f61ae17db2
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-05-06 18:27:16 +0300

    hit_sphere: Return distance

--- raytrace.c
+++ raytrace.c
@@ -6,27 +6,30 @@
 #include "ray.h"
 #include "vecmat/vec3f.h"
 
-bool hit_sphere(struct vec3f center, float r, struct ray ray)
+float hit_sphere(struct vec3f center, float r, struct ray ray)
 {
     struct vec3f oc = vec3f_sub(ray.orig, center);
     float a = vec3f_dot(ray.dir, ray.dir);
     float b = 2.0f * vec3f_dot(oc, ray.dir);
     float c = vec3f_dot(oc, oc) - r * r;
     float discriminant = b*b - 4 * a * c;
-    return (discriminant > 0.0f);
+    if (discriminant < 0.0f)
+        return -1.0f;
+    return (-b - sqrtf(discriminant)) / (2.0f * a);
 }
 
 static struct color get_ray_color(struct ray ray)
 {
     struct vec3f unit_dir = vec3f_normalized(ray.dir);
-    float t = 0.5f * (unit_dir.y + 1.0f);
-    float ti = 1.0f - t;
+    float t;
     struct vec3f c;
 
-    if (hit_sphere(vec3f_init(0, 0, -1), 0.5, ray))
+    t = hit_sphere(vec3f_init(0, 0, -1), 0.5, ray);
+    if (t > 0.0f)
         return (struct color) { 0.2f, 0.3f, 0.5f };
 
-    c = vec3f_add(vec3f_init(ti, ti, ti),
+    t = 0.5f * (unit_dir.y + 1.0f);
+    c = vec3f_add(vec3f_init(1.0f - t, 1.0f - t, 1.0f - t),
                   vec3f_muls(vec3f_init(0.5f, 0.7f, 1.0f), t));
 
     return (struct color){ c.x, c.y, c.z, 1.0f };
