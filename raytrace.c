3f8cd150e44f2e59972d32a4d64707514e36eda6
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-05-20 22:50:34 +0300

    Render diffuse spheres

--- raytrace.c
+++ raytrace.c
@@ -14,8 +14,8 @@ struct sphere {
     float r;
 } spheres[] = {
     {
-        .orig = { -0.5f, 0.0f, -0.5f },
-        .r = 0.5f,
+        .orig = { 0.0f, -7.5f, -1.5f },
+        .r = 7.0f,
     }, {
         .orig = { 0.0f, 0.0f, -1.0f },
         .r = 0.5f,
@@ -67,26 +67,57 @@ static bool test_hit(struct ray ray, struct hit *hit)
     return false;
 }
 
-static struct color get_ray_color(struct ray ray)
+static inline struct vec3f random_in_unit_sphere(void)
+{
+    struct vec3f p;
+
+    do {
+        p = vec3f_muls(vec3f_sub(vec3f_init(drand48(), drand48(), drand48()),
+                                 vec3f_ones), 2.0f);
+    } while (vec3f_norm2(p) >= 1.0f);
+
+    return p;
+}
+
+/* TODO These break strict aliasing */
+static inline struct color color_add(struct color a, struct color b)
+{
+    struct vec3f s = vec3f_add(*(struct vec3f *)&a, *(struct vec3f *)&b);
+    return *(struct color *)&s;
+}
+
+static inline struct color color_muls(struct color c, float x)
+{
+    struct vec3f s = vec3f_muls(*(struct vec3f *)&c, x);
+    return *(struct color *)&s;
+}
+
+static struct color trace(struct ray ray, int n_bounce)
 {
     struct vec3f unit_dir = vec3f_normalized(ray.dir);
-    struct vec3f c;
+    struct vec3f target;
+    struct color c;
+    struct vec3f cm;
     struct hit hit;
     bool is_hit;
     float t;
+    const int max_bounce = 10;
 
     is_hit = test_hit(ray, &hit);
-    if (is_hit) {
-        c = vec3f_muls(vec3f_adds(hit.n, 1.0f), 0.5f);
-        return (struct color) { c.x, c.y, c.z };
+    if (is_hit && n_bounce < max_bounce) {
+        target = vec3f_add3(hit.p, hit.n, random_in_unit_sphere());
+        ray.orig = hit.p;
+        ray.dir = vec3f_sub(target, hit.p);
+        c = trace(ray, n_bounce + 1);
+        return color_muls(c, 0.5f);
     }
 
     /* Background color */
     t = 0.5f * (unit_dir.y + 1.0f);
-    c = vec3f_add(vec3f_init(1.0f - t, 1.0f - t, 1.0f - t),
-                  vec3f_muls(vec3f_init(0.5f, 0.7f, 1.0f), t));
+    cm = vec3f_add(vec3f_init(1.0f - t, 1.0f - t, 1.0f - t),
+                   vec3f_muls(vec3f_init(0.5f, 0.7f, 1.0f), t));
 
-    return (struct color){ c.x, c.y, c.z, 1.0f };
+    return (struct color){ cm.x, cm.y, cm.z, 1.0f };
 }
 
 struct camera {
@@ -119,12 +150,6 @@ struct ray camera_gen_ray(struct camera *cam, float u, float v)
     return ray;
 }
 
-struct color color_add(struct color a, struct color b)
-{
-    struct vec3f s = vec3f_add(*(struct vec3f *)&a, *(struct vec3f *)&b);
-    return *(struct color *)&s;
-}
-
 int main(void)
 {
     struct camera cam;
@@ -146,7 +171,7 @@ int main(void)
                 u = (x + drand48()) / fb.w;
                 v = (y + drand48()) / fb.h;
                 ray = camera_gen_ray(&cam, u, v);
-                c = color_add(c, get_ray_color(ray));
+                c = color_add(c, trace(ray, 0));
             }
             c = (struct color) { c.r / ns, c.g / ns, c.b / ns };
             fb_putcolor(&fb, x, y, c);
